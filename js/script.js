28e582fe9877ab893b3c177203ac3fb64ebe6662
DoPhucKhang <khang241205@gmail.com>
2026-06-20 14:36:59 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,75 @@
-document.getElementById("btn").addEventListener("click", function () {
-    alert("Xin chào từ External JavaScript!");
+// Ẩn hiện nội dung
+
+const btnShow = document.getElementById("btnShow");
+
+const content = document.getElementById("content");
+
+
+btnShow.addEventListener("click", function(){
+
+    if(content.style.display === "none"){
+
+        content.style.display = "block";
+
+    }else{
+
+        content.style.display = "none";
+
+    }
+
+});
+
+
+
+
+// Kiểm tra form
+
+const form = document.getElementById("contactForm");
+
+const result = document.getElementById("result");
+
+
+form.addEventListener("submit", function(event){
+
+
+    event.preventDefault();
+
+
+    const name =
+    document.getElementById("name").value;
+
+
+    const email =
+    document.getElementById("email").value;
+
+
+    const message =
+    document.getElementById("message").value;
+
+
+
+    if(
+        name === "" ||
+        email === "" ||
+        message === ""
+    ){
+
+        result.textContent =
+        "Vui lòng nhập đầy đủ thông tin!";
+
+        result.style.color="red";
+
+
+    }else{
+
+
+        result.textContent =
+        "Gửi liên hệ thành công!";
+
+        result.style.color="green";
+
+
+    }
+
+
 });
